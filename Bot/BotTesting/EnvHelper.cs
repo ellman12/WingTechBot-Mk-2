1da2021e6441af50b9e16e071b85beb9fb8c9fdd
Elliott DuCharme <ellduc4@gmail.com>
2024-12-27 14:07:39 -0600

    Create EnvHelper in test project

---
+++ Bot/BotTesting/EnvHelper.cs
@@ -0,0 +1,21 @@
+namespace BotTesting;
+
+///Helper methods for working with the .env file.
+public static class EnvHelper
+{
+	public static string EnvPath { get; } = Path.Combine(Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.FullName, ".env");
+	
+	///Reads in and sets the values from the .env file.
+	public static void ReadEnvVariables()
+	{
+		if (!File.Exists(EnvPath))
+			return;
+		
+		string[] lines = File.ReadAllLines(EnvPath);
+		foreach (var line in lines)
+		{
+			string[] split = line.Split('=', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+			Environment.SetEnvironmentVariable(split[0], split[1]);
+		}
+	}
+}
