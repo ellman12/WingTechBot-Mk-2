a0784c8b8d004ac896f49eb7304efe159b2a2033
Elliott DuCharme <ellduc4@gmail.com>
2024-12-28 21:09:02 -0600

    Add SetUp method to ModelTests

--- Bot/BotTesting/DatabaseTests/ModelTests/ModelTests.cs
+++ Bot/BotTesting/DatabaseTests/ModelTests/ModelTests.cs
@@ -2,4 +2,18 @@ namespace BotTesting.DatabaseTests.ModelTests;
 
 ///Represents any test for database models.
 [TestFixture]
-public abstract class ModelTests {}
+public abstract class ModelTests
+{
+	[SetUp]
+	public async Task SetUp()
+	{
+		await RecreateDatabase();
+	}
+
+	private static async Task RecreateDatabase()
+	{
+		await using BotDbContext context = new();
+		await context.Database.EnsureDeletedAsync();
+		await context.Database.EnsureCreatedAsync();
+	}
+}
