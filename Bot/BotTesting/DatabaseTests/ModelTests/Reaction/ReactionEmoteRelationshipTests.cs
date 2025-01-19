93687e6350e126718eadc2ea99cd13351f73eec5
Elliott DuCharme <ellduc4@gmail.com>
2025-01-19 17:18:49 -0600

    Add tests for Reaction/ReactionEmote foreign keys

---
+++ Bot/BotTesting/DatabaseTests/ModelTests/Reaction/ReactionEmoteRelationshipTests.cs
@@ -0,0 +1,14 @@
+namespace BotTesting.DatabaseTests.ModelTests.Reaction;
+using Reaction=WingTechBot.Database.Models.Reaction;
+
+[TestFixture]
+public sealed class ReactionEmoteRelationshipTests : ModelTests
+{
+	[TestCase(123456ul, 123ul, 456ul, "upvote", 565656ul, 1)]
+	public async Task FindReferencedReactionEmote(ulong giverId, ulong receiverId, ulong messageId, string emoteName, ulong discordEmoteId, int emoteId)
+	{
+		await Reaction.AddReaction(giverId, receiverId, messageId, emoteName, discordEmoteId);
+		var newReaction = await Reaction.Find(giverId, receiverId, messageId, emoteId);
+		Assert.True(newReaction != null && newReaction.Emote != null);
+	}
+}
---
+++ Bot/BotTesting/DatabaseTests/ModelTests/ReactionEmote/ReactionRelationshipTests.cs
@@ -0,0 +1,23 @@
+namespace BotTesting.DatabaseTests.ModelTests.ReactionEmote;
+using ReactionEmote=WingTechBot.Database.Models.ReactionEmote;
+using Reaction=WingTechBot.Database.Models.Reaction;
+
+[TestFixture]
+public sealed class ReactionRelationshipTests : ModelTests
+{
+	[TestCase("upvote", 123456ul, 123ul, 456ul, 789ul)]
+	public async Task FindReferencedReactions(string emoteName, ulong discordEmoteId, ulong giverId, ulong receiverId, ulong messageId)
+	{
+		await ReactionEmote.AddEmote(emoteName, discordEmoteId);
+		var newEmote = await ReactionEmote.Find(emoteName, discordEmoteId);
+		Assert.NotNull(newEmote);
+
+		foreach (int i in Enumerable.Range(1, 4))
+		{
+			messageId++;
+			await Reaction.AddReaction(giverId, receiverId, messageId, emoteName, discordEmoteId);
+			newEmote = await ReactionEmote.Find(emoteName, discordEmoteId); //Refresh its data.
+			Assert.True(newEmote != null && newEmote.Reactions.Count == i);
+		}
+	}
+}
