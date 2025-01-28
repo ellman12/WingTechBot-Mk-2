40e72463ce9309c40d13b5d73174354c95cdbc8b
Elliott DuCharme <ellduc4@gmail.com>
2025-01-28 00:52:28 -0600

    Clean up /gato-top message formatting

--- Bot/WingTechBot/Commands/Gatos/GatoTopCommand.cs
+++ Bot/WingTechBot/Commands/Gatos/GatoTopCommand.cs
@@ -27,26 +27,25 @@ public sealed class GatoTopCommand : SlashCommand
 
 		var gatoLeaderboard = await Gato.GetGatoLeaderboard();
 
-		string message;
-		if (gatoLeaderboard.Length > 0)
+		if (!gatoLeaderboard.Any())
 		{
-			var entries = gatoLeaderboard.Aggregate((rankings: new List<(int rank, string name, int count)>(), lastCount: 0, index: 1, lastRank: 0), (current, entry) =>
+			await command.FollowupAsync("No gatos found");
+			return;
+		}
+
+		var entries = gatoLeaderboard.Aggregate(
+			(rankings: new List<(int rank, string name, int count)>(), lastCount: 0, index: 1, lastRank: 0),
+			(current, entry) =>
 			{
 				var (rankings, lastCount, index, lastRank) = current;
 				int newRank = entry.count == lastCount ? lastRank : index;
 
 				rankings.Add((newRank, entry.name, entry.count));
 				return (rankings, entry.count, index + 1, newRank);
-			}, tuple => tuple.rankings)
+			},
+			tuple => tuple.rankings)
 			.Select(entry => $"{entry.rank.ToString(),-6} {entry.count.ToString(),-7} {entry.name}");
 
-			message = $"```Gato Leaderboard\nRank   Count   Name\n{String.Join('\n', entries)}```";
-		}
-		else
-		{
-			message = "No gatos found";
-		}
-
-		await command.FollowupAsync(message);
+		await command.FollowupAsync($"```Gato Leaderboard\nRank   Count   Name\n{String.Join('\n', entries)}```");
 	}
 }
