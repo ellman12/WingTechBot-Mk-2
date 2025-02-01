c9adbc3362bf03b3171bee25da352ff1147e0a80
Elliott DuCharme <ellduc4@gmail.com>
2025-02-01 14:06:33 -0600

    Add words.txt

--- Bot/WingTechBot/Games/Game.cs
+++ Bot/WingTechBot/Games/Game.cs
@@ -10,6 +10,8 @@ public abstract class Game
 	public List<IUser> Players { get; set; }
 
 	public virtual uint? MaxPlayers { get; protected set; } = null;
+	///List of words that can be used for games like <see cref="Hangman"/>
+	protected string[] Words { get; } = File.ReadAllLines(Path.Combine(Program.ProjectRoot, "Games", "words.txt"));
 
 	///Any setup this game requires. E.g., number of players, etc.
 	protected abstract Task GameSetup();
