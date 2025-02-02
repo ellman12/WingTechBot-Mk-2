90ed031cd59a528b8c6eeeb5f01c91248e18f21d
Elliott DuCharme <ellduc4@gmail.com>
2025-02-02 13:59:44 -0600

    UserInput.Prompt now accepts a Predicate condition

--- Bot/WingTechBot/UserInput.cs
+++ Bot/WingTechBot/UserInput.cs
@@ -2,8 +2,8 @@ namespace WingTechBot;
 
 public static class UserInput
 {
-	///Prompts a text or thread channel for a message of a type.
-	public static async Task<T> Prompt<T>(SocketTextChannel channel, string prompt, CancellationToken token)
+	///Prompts a text or thread channel for a message of a type, optionally with a condition.
+	public static async Task<T> Prompt<T>(SocketTextChannel channel, string prompt, CancellationToken token, Predicate<T> condition = null)
 	{
 		await channel.SendMessageAsync(prompt);
 		T value = default;
@@ -20,6 +20,13 @@ public static class UserInput
 			try
 			{
 				value = (T)Convert.ChangeType(messageReceived.Content.ToLower(), typeof(T));
+
+				if (condition != null && !condition(value))
+				{
+					await Task.Delay(100, token);
+					continue;
+				}
+
 				break;
 			}
 			catch (Exception e)
@@ -31,7 +38,10 @@ public static class UserInput
 		return value;
 	}
 
-	public static async Task<string> Prompt(SocketTextChannel channel, string prompt, CancellationToken token) => await Prompt<string>(channel, prompt, token);
+	public static async Task<string> Prompt(SocketTextChannel channel, string prompt, CancellationToken token, Predicate<string> condition = null)
+	{
+		return await Prompt<string>(channel, prompt, token, condition);
+	}
 
 	private static bool ValidUserMessage(IMessage message) => !message.Author.IsBot && !message.Author.IsWebhook;
 }
