55d112918c71370b15864f4e8aba615937556f16
Elliott DuCharme <ellduc4@gmail.com>
2025-02-03 18:24:45 -0600

    Start to gameplay loop of Hangman

--- Bot/WingTechBot/Games/Hangman/Hangman.cs
+++ Bot/WingTechBot/Games/Hangman/Hangman.cs
@@ -2,17 +2,21 @@ namespace WingTechBot.Games.Hangman;
 
 public sealed class Hangman : Game
 {
-	private string word = "", check = "";
-	private readonly List<char> guesses = [];
+	private string word = "";
+	private readonly List<char> correctLetters = [], incorrectLetters = [];
 	private readonly List<string> wordGuesses = [];
 
 	private Dictionary<IUser, int> scores;
 
-	private int currentHostIndex = -1, strikes = 0, score = 0, total = 0;
+	private int currentHostIndex = -1;
 
 	private bool pvp;
 
 	private IUser CurrentHost => Players[currentHostIndex];
+
+	private int Strikes => incorrectLetters.Count + wordGuesses.Count;
+
+	private const int StrikeLimit = 6;
 
 	private static readonly string[] heads =
 	[
@@ -60,9 +64,9 @@ public sealed class Hangman : Game
 		{
 			scores = new Dictionary<IUser, int>();
 
-			foreach (var id in Players)
+			foreach (var player in Players)
 			{
-				scores.Add(id, 0);
+				scores.Add(player, 0);
 			}
 		}
 	}
@@ -71,11 +75,20 @@ public sealed class Hangman : Game
 	{
 		while (true)
 		{
+			await RoundSetup();
 			await RunRound();
+
+			string input = await UserInput.Prompt(ThreadChannel, "Type \"next\" to continue or \"end\" to stop playing.", CancelTokenSource.Token, s => s.Trim().ToLower() is "next" or "end");
+
+			if (input == "end")
+			{
+				await SendMessage("Game finished");
+				break;
+			}
 		}
 	}
 
-	private async Task RunRound()
+	private async Task RoundSetup()
 	{
 		if (pvp)
 		{
@@ -88,7 +101,63 @@ public sealed class Hangman : Game
 			word = Words[Random.Shared.Next(0, Words.Length)].Trim().ToUpper();
 		}
 
-		;
+		correctLetters.Clear();
+		incorrectLetters.Clear();
+		wordGuesses.Clear();
+
+		await SendMessage(GetScreen());
+
+		Logger.LogLine($"Starting round of Hangman with the word {word}");
+	}
+
+	private async Task RunRound()
+	{
+		while (true)
+		{
+			string output = "";
+
+			if (Strikes == StrikeLimit)
+			{
+				await SendMessage($"Game over! The word was: {word}");
+				break;
+			}
+
+			if (word.All(c => correctLetters.Contains(c)))
+			{
+				break;
+			}
+
+			string input = (await UserInput.Prompt(ThreadChannel, "Guess a letter or word", CancelTokenSource.Token)).ToUpper();
+
+			if (input.Length > 1)
+			{
+				if (input == word)
+				{
+					output = "Correct!";
+					correctLetters.AddRange(word.Distinct());
+				}
+				else
+				{
+					wordGuesses.Add(input);
+				}
+			}
+			else
+			{
+				char letter = input[0];
+				if (word.Contains(letter))
+				{
+					correctLetters.Add(letter);
+					output = "Correct!";
+				}
+				else
+				{
+					incorrectLetters.Add(letter);
+				}
+			}
+
+			output += GetScreen();
+			await SendMessage(output);
+		}
 	}
 
 	private void AdvanceHost()
@@ -99,4 +168,48 @@ public sealed class Hangman : Game
 	}
 
 	protected override async Task ProcessMessage(SocketMessage message) {}
+
+	private string GetScreen()
+	{
+		string message =
+			$"""
+			```
+			||-------
+			||      |
+			||    {Head}
+			||    {Body}
+			||    {Legs}
+			||
+			||
+			====[HANGMAN]====
+			RIGHT: {String.Join(' ', word.Select(c => word.Contains(c) && correctLetters.Contains(c) ? c : '_'))}
+			WRONG: {String.Join(' ', incorrectLetters)}
+			{String.Join("\n", wordGuesses)}
+			```
+			""";
+
+		return message;
+	}
+
+	private string Head => Strikes switch
+	{
+		1 => heads[0],
+		>= 2 => heads[Random.Shared.Next(heads.Length)],
+		_ => "     "
+	};
+
+	private string Body => Strikes switch
+	{
+		2 => "  8  ",
+		3 => "  8 -",
+		>= 4 => "- 8 -",
+		_ => "     "
+	};
+
+	private string Legs => Strikes switch
+	{
+		5 => " /  ",
+		6 => @" / \",
+		_ => "   "
+	};
 }
