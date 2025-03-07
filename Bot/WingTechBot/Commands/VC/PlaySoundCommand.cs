64e59cb31a9136ebed92dab760f34a1cc0654789
Elliott DuCharme <ellduc4@gmail.com>
2025-03-07 13:04:42 -0600

    /play-sound now has amount and delay options

--- Bot/WingTechBot/Commands/VC/PlaySoundCommand.cs
+++ Bot/WingTechBot/Commands/VC/PlaySoundCommand.cs
@@ -6,12 +6,26 @@ public sealed class PlaySoundCommand : SlashCommand
 	{
 		return new SlashCommandBuilder()
 			.WithName("play-sound")
-			.WithDescription("Make WTB join a specific VC channel")
+			.WithDescription("Make WTB play a sound from the soundboard.")
 			.AddOption(new SlashCommandOptionBuilder()
 				.WithName("name")
 				.WithDescription("The name of the sound")
 				.WithType(ApplicationCommandOptionType.String)
 				.WithRequired(true)
+			)
+			.AddOption(new SlashCommandOptionBuilder()
+				.WithName("amount")
+				.WithDescription("How many times to play the sound.")
+				.WithRequired(false)
+				.WithType(ApplicationCommandOptionType.Integer)
+				.WithMinValue(1)
+			)
+			.AddOption(new SlashCommandOptionBuilder()
+				.WithName("delay")
+				.WithDescription("Delay in ms between each sound. Only used if amount specified.")
+				.WithRequired(false)
+				.WithType(ApplicationCommandOptionType.Integer)
+				.WithMinValue(100)
 			);
 	}
 
@@ -20,7 +34,8 @@ public sealed class PlaySoundCommand : SlashCommand
 		if (command.CommandName != Name)
 			return;
 
-		var name = command.Data.Options.Single(o => o.Name == "name").Value as string;
+		var options = command.Data.Options;
+		var name = options.Single(o => o.Name == "name").Value as string;
 		if (String.IsNullOrWhiteSpace(name))
 		{
 			await command.FollowupAsync("Invalid sound name");
@@ -28,6 +43,8 @@ public sealed class PlaySoundCommand : SlashCommand
 		}
 
 		var sound = Bot.VoiceChannelConnection.Sounds.FirstOrDefault(s => s.Name.Contains(name, StringComparison.InvariantCultureIgnoreCase));
+		var amount = options.FirstOrDefault(o => o.Name == "amount")?.Value as long? ?? 1;
+		var delay = options.FirstOrDefault(o => o.Name == "delay")?.Value as long? ?? 1000;
 		if (sound == null)
 		{
 			await command.FollowupAsync("Invalid sound name");
@@ -41,10 +58,17 @@ public sealed class PlaySoundCommand : SlashCommand
 		}
 		else
 		{
-			await command.FollowupAsync($"Playing sound \"{sound.Name}\"");
-
+			await command.FollowupAsync($"Playing sound \"{sound.Name}\" {(amount > 1 ? $"{amount} times, with delay of {delay} ms" : "")}");
 			var data = new {sound_id = sound.SoundId};
-			await Bot.VoiceChannelConnection.Client.PostAsync($"https://discord.com/api/v10/channels/{channel.Id}/send-soundboard-sound", new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, "application/json"));
+
+			_ = Task.Run(async () =>
+			{
+				for (int i = 0; i < amount; i++)
+				{
+					await Bot.VoiceChannelConnection.Client.PostAsync($"https://discord.com/api/v10/channels/{channel.Id}/send-soundboard-sound", new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, "application/json"));
+					await Task.Delay(TimeSpan.FromMilliseconds(delay));
+				}
+			});
 		}
 	}
 }
