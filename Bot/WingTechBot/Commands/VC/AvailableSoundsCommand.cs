ef0abedd64c1a388441226ebafc07e120aca8b93
Elliott DuCharme <ellduc4@gmail.com>
2025-03-13 18:53:44 -0500

    /available-sounds can now hide the server names in its output

--- Bot/WingTechBot/Commands/VC/AvailableSoundsCommand.cs
+++ Bot/WingTechBot/Commands/VC/AvailableSoundsCommand.cs
@@ -6,18 +6,34 @@ public sealed class AvailableSoundsCommand : SlashCommand
 
 	protected override SlashCommandBuilder CreateCommand()
 	{
-		return new SlashCommandBuilder().WithName("available-sounds").WithDescription("Lists all the sounds that can be played");
+		return new SlashCommandBuilder()
+			.WithName("available-sounds")
+			.WithDescription("Lists all the sounds that can be played")
+			.AddOption(new SlashCommandOptionBuilder()
+				.WithName("show-server-names")
+				.WithDescription("Shows the servers each sound comes from")
+				.WithType(ApplicationCommandOptionType.Boolean)
+				.WithRequired(false)
+			);
 	}
 
 	public override async Task HandleCommand(SocketSlashCommand command)
 	{
-		var groups = Bot.VoiceChannelConnection.AvailableSounds.GroupBy(s => s.GuildId);
-
 		string message = "";
-		foreach (var group in groups)
+
+		var showServers = command.Data.Options.SingleOrDefault(o => o.Name == "show-server-names")?.Value as bool?;
+		if (showServers is not null and not false)
 		{
-			message += $"### {(group.Key == null ? "Default" : Bot.Client.GetGuild(ulong.Parse(group.Key)).Name)}\n";
-			message += $"{String.Join("\n", group.Select(s => $"* {s.Name}"))}\n";
+			var groups = Bot.VoiceChannelConnection.AvailableSounds.GroupBy(s => s.GuildId);
+			foreach (var group in groups)
+			{
+				message += $"### {(group.Key == null ? "Default" : Bot.Client.GetGuild(ulong.Parse(group.Key)).Name)}\n";
+				message += $"{String.Join("\n", group.Select(s => $"* {s.Name}"))}\n";
+			}
+		}
+		else
+		{
+			message = String.Join('\n', Bot.VoiceChannelConnection.AvailableSounds.Select(s => s.Name));
 		}
 
 		await command.RespondAsync(message, ephemeral: true);
