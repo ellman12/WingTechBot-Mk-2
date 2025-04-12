5c9283f43dec6f2352f5f80325562412a32088e9
Elliott DuCharme <ellduc4@gmail.com>
2025-04-12 10:32:35 -0500

    Simplify BuildAndRunBot.sh

--- Bot/WingTechBot/Bash Scripts/BuildAndRunBot.sh
+++ Bot/WingTechBot/Bash Scripts/BuildAndRunBot.sh
@@ -1,28 +1,4 @@
-# Automatically starts Docker, pulls master (or the branch you give it), and builds/starts the bot.
-
-if [ -z "$1" ]; then
-	branch="master"
-else
-	branch=$1
-fi
-
-clear
-
-cd ~
-
 sudo systemctl start docker
 
-if [ -d "./WingTechBot" ]; then
-	cd WingTechBot
-	git switch $branch
-	git fetch
-	git reset --hard origin/$branch
-else
-	git clone https://github.com/ellman12/WingTechBot
-	cd WingTechBot
-fi
+docker build --no-cache -t wingtech-bot . && docker-compose up -d --force-recreate
 
-cd Bot/WingTechBot
-
-# -d can be used to hide compose's output.
-docker build --no-cache -t wingtech-bot . && docker-compose up --force-recreate
