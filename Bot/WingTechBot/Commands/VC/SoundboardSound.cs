823f55440abf93a0b649d1d8556aaaac3a9bb5d4
Elliott DuCharme <ellduc4@gmail.com>
2025-04-24 15:04:51 -0500

    Clean up SoundboardSound.cs

--- Bot/WingTechBot/Commands/VC/SoundboardSound.cs
+++ Bot/WingTechBot/Commands/VC/SoundboardSound.cs
@@ -1,28 +1,14 @@
 namespace WingTechBot.Commands.VC;
 
+///Represents a sound &lt;= 5 seconds played through Discord's Soundboard feature.
 public sealed class SoundboardSound
 {
 	[JsonPropertyName("name")]
-	public string Name { get; set; }
+	public string Name { get; private init; }
 
 	[JsonPropertyName("sound_id"), JsonConverter(typeof(StringUInt64Converter))]
-	public ulong SoundId { get; set; }
-
-	// [JsonPropertyName("volume")]
-	// public double Volume { get; set; }
-	//
-	// [JsonPropertyName("emoji_id")]
-	// public ulong? EmojiId { get; set; }
-	//
-	// [JsonPropertyName("emoji_name")]
-	// public string EmojiName { get; set; }
+	public ulong SoundId { get; private init; }
 
 	[JsonPropertyName("guild_id"), JsonConverter(typeof(StringUInt64Converter))]
-	public ulong? GuildId { get; set; }
-
-	// [JsonPropertyName("available")]
-	// public bool Available { get; set; }
-
-	// [JsonPropertyName("user")]
-	// public SocketGuildUser User { get; set; }
+	public ulong? GuildId { get; private init; }
 }
