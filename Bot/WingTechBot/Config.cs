eaaed9d9dacc4f21c8f783528739ec0eac29368b
Elliott DuCharme <ellduc4@gmail.com>
2025-04-26 11:59:26 -0500

    Remove password from Config.cs

--- Bot/WingTechBot/Config.cs
+++ Bot/WingTechBot/Config.cs
@@ -12,8 +12,6 @@ public sealed record Config
 
 	public string ServerUrl { get; init; }
 
-	public string SoundboardPassword { get; init; }
-
 	///The IDs of Discord servers the bot can get sounds from.
 	public ulong[] SoundboardServerIds { get; init; }
 
