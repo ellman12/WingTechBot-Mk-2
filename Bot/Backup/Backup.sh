788d7c88bb15c8d9374853479987749510df0118
Elliott DuCharme <ellduc4@gmail.com>
2025-05-07 19:24:46 -0500

    Backups older than 14 days are auto-deleted

--- Bot/Backup/Backup.sh
+++ Bot/Backup/Backup.sh
@@ -9,3 +9,7 @@ pg_dump -h $DATABASE_HOST -U $DATABASE_USER $DATABASE_NAME | gzip > $BACKUP_FILE
 
 echo "Backup complete"
 
+find /backups -name "${DATABASE_NAME}_*.sql.gz" -type f -mtime +14 -exec rm {} \;
+
+echo "Deleted backups older than 14 days"
+
